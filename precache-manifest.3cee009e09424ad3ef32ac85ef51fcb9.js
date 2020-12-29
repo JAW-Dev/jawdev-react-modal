95b94a1e7cf7723feea0f97ecfe82f4d58f280c4
Jason Witt <jawittdesigns@gmail.com>
2020-12-29 12:22:36 -0500

    Updates

---
+++ precache-manifest.3cee009e09424ad3ef32ac85ef51fcb9.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2fd687eef4e6d1fb5b0c2f90b69cfab",
+    "url": "./index.html"
+  },
+  {
+    "revision": "51c4c57ac6e97770a610",
+    "url": "./static/css/2.2f475074.chunk.css"
+  },
+  {
+    "revision": "335959b06f25364cb7a8",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "51c4c57ac6e97770a610",
+    "url": "./static/js/2.34d9c01c.chunk.js"
+  },
+  {
+    "revision": "a4cd4e92ecb4bcf9d897f2570db69384",
+    "url": "./static/js/2.34d9c01c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "335959b06f25364cb7a8",
+    "url": "./static/js/main.91695ca2.chunk.js"
+  },
+  {
+    "revision": "59bd054f3bde6b0e219b610549d13f6f",
+    "url": "./static/js/main.91695ca2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df8821a05100852eae78",
+    "url": "./static/js/runtime-main.36784a85.js"
+  },
+  {
+    "revision": "6a47133378275f52f65cb1b0daa350cd",
+    "url": "./static/media/fira-code-all-400-normal.6a471333.woff"
+  },
+  {
+    "revision": "50f767c5e2e184d2c58a510e8e7e3b64",
+    "url": "./static/media/fira-code-cyrillic-400-normal.50f767c5.woff2"
+  },
+  {
+    "revision": "d32a2f96078d9b549d75599f1097ba71",
+    "url": "./static/media/fira-code-cyrillic-ext-400-normal.d32a2f96.woff2"
+  },
+  {
+    "revision": "bb28833a1b3ee46c90cf05d3728d1d83",
+    "url": "./static/media/fira-code-greek-400-normal.bb28833a.woff2"
+  },
+  {
+    "revision": "eafc069988f6fd7d0385746c962122af",
+    "url": "./static/media/fira-code-greek-ext-400-normal.eafc0699.woff2"
+  },
+  {
+    "revision": "c6f7f2bfaccffae1d24d6945b822791e",
+    "url": "./static/media/fira-code-latin-400-normal.c6f7f2bf.woff2"
+  },
+  {
+    "revision": "03c248ada875e694ddd54e29afdcfce3",
+    "url": "./static/media/fira-code-latin-ext-400-normal.03c248ad.woff2"
+  }
+]);
